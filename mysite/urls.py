02cdee1f7ee0c0b6a859ec3c233007bcac3e706a
yanwarsolahudinn <yanwarsolahudinn@gmail.com>
2015-09-02 02:33:20 +0700

    Adding login users.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+admin.autodiscover()
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'', include('blog.urls')),
 ]
 
