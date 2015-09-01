bf695c344ab5414379dcbd225c4fa8dd159cb4b9
yanwarsolahudinn <yanwarsolahudinn@gmail.com>
2015-09-02 03:24:47 +0700

    Adding Logout and serving static files (assets) for admin, when debug False in settings.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,12 +1,16 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
-admin.autodiscover()
+#admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page':'/'}),
     url(r'', include('blog.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
 handler404 = 'blog.views.handler404'
