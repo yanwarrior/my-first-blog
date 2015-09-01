60632a18ef78518bec7bac77040b298b7e1272a6
yanwarsolahudinn <yanwarsolahudinn@gmail.com>
2015-09-01 20:32:58 +0700

    Adding page with list of unpublished posts

--- blog/views.py
+++ blog/views.py
@@ -48,5 +48,7 @@ def post_edit(request, pk):
 	return render(request, 'blog/post_edit.html', {'form':form})
 
 
-	
+def post_draft_list(request):
+	posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+	return render(request, 'blog/post_draft_list.html', {'posts':posts})	
 
