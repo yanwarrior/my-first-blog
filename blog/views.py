76c77b2d24896398b62fd121c52ccd38362a3fa8
yanwarsolahudinn <yanwarsolahudinn@gmail.com>
2015-09-01 21:15:38 +0700

    Update post detail.

--- blog/views.py
+++ blog/views.py
@@ -50,5 +50,9 @@ def post_edit(request, pk):
 
 def post_draft_list(request):
 	posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
-	return render(request, 'blog/post_draft_list.html', {'posts':posts})	
+	return render(request, 'blog/post_draft_list.html', {'posts':posts})
 
+def post_publish(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.publish()
+	return redirect('blog.views.post_detail', pk=pk)	
